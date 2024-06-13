770ffe612e5e9fcb4938aecfa2b05e79b37af700
Keith Nickas <keithnickas@gmail.com>
2024-06-13 11:42:38 -0700

    fix(deem-flights): grammar updates

--- app/code/deem/flight-results/page.tsx
+++ app/code/deem/flight-results/page.tsx
@@ -8,27 +8,32 @@ export default function FlightResults() {
           Deem Flight Results Prototype
         </h1>
         <p className="text-white lg:text-lg pb-6">
-          <span className="font-bold">Challenge</span>: Provide an experience
-          that surfaces pertinent information in a simplistic, cohesive manner
-          which minimizes any potential confusion and the intent of the
-          information to direct the user to make the appropriate choices based
-          on corporate and personal needs.
+          <span className="font-bold italic">Challenge:</span>
+          <br />
+          Provide an experience that surfaces pertinent information in a
+          simplistic, cohesive manner which minimizes any potential confusion
+          and the intent of the information is to direct the user to make the
+          appropriate choices based on corporate and personal needs.
         </p>
         <p className="text-white lg:text-lg pb-6">
-          <span className="font-bold">Other challenges</span>: Complex flight(s)
-          where more than one connection, multiple carriers, or, flight tiers
-          could be displayed within a single flight object.
+          <span className="font-bold italic">Other challenges:</span>
+          <br />
+          Display Complex flights where more than one connection, multiple carriers,
+          or flight tiers could be displayed within a single flight object.
         </p>
         <p className="text-white lg:text-lg pb-6">
-          <span className="font-bold">Requirements</span>: Encompassing all
-          existing features of the flight application, specifically surrounding
-          fare tiers, policy, dynamic filtering, fare comparison.
+          <span className="font-bold italic">Requirements:</span>
+          <br />
+          Encompass all existing features of the flight application,
+          specifically surrounding fare tiers, policy, dynamic filtering, and fare
+          comparison.
         </p>
         <p className="text-white lg:text-lg pb-6">
-          <span className="font-bold">Research</span>
+          <span className="font-bold italic">Research:</span>
+          <br />
           Reviewed Google Flights, American, Alaska, Southwest, Delta, and
-          United airlines customer-facing websites. Additionally reviewed
-          third-party consumer sites such as Expedia, Kayak, Kiwi and others to
+          United airlines' customer-facing websites. Additionally, reviewed
+          third-party consumer sites such as Expedia, Kayak, Kiwi, and others to
           gain further insight into the user experience.
         </p>
         <h2 className="h2 font-extrabold text-white lg:text-2xl pb-6">
@@ -54,7 +59,7 @@ export default function FlightResults() {
         </p>
         <div className="flex column gap-20">
           <div className="flex flex-wrap content-start basis-1/3">
-            <span className="font-bold italic">Filters UX</span> Add option for
+            <span className="font-bold italic">Filters UX:</span> Add option for
             displaying fare tier, stops, flight times, and, other filter options
             that could be configured to be displayed above the flight objects.
             The chevron icons ({`< / >`}) are used for stepping of predefined
@@ -62,15 +67,15 @@ export default function FlightResults() {
             hovering the filter title to expand the available options.
           </div>
           <div className="flex flex-wrap content-start basis-1/3">
-            <span className="font-bold italic">Use Case</span> When a user has
+            <span className="font-bold italic">Use Case:</span> When a user has
             selected a stepping, all the flight objects would update to display
-            the appropriate options. Let’s say the user has used Flight Times
+            the appropriate options. Let’s say the user has used Flight Times,
             which is stepped (up or down), the flight objects that match that
             stepping would be displayed and others not matching would be hidden
             from view.
           </div>
           <div className="flex flex-wrap content-start basis-1/3">
-            <span className="font-bold italic">Flight Object UX</span> In this
+            <span className="font-bold italic">Flight Object UX:</span> In this
             view you see informational icons, textual color treatments such as a
             different arrival airport (orange) than originally searched for, out
             of policy (red ban icon), extended layover (warning icon) and
@@ -79,24 +84,24 @@ export default function FlightResults() {
         </div>
         <div className="flex flex-wrap content-start py-6">
           <p className="text-white lg:text-lg pb-6">
-            <span className="font-bold italic">Enhancements</span>
+            <span className="font-bold italic">Enhancements:</span>
             <br />
-            Add option for displaying fare tier “tab” upfront. Two styles were
+            Add an option for displaying fare tier “tab” upfront. Two styles were
             developed based on input from stakeholders:
           </p>
           <ol className="list-decimal pl-8">
             <li>
               To display the pertinent flight object info, and then displaying
-              available flight tiers below
+              available flight tiers below.
             </li>
             <li>
               Display flight tiers at the top by maintaining the existing
-              experience
+              experience.
             </li>
             <li>
               Increase separation of the flight objects, while maintaining the
               ability to show more options to the user to reduce scrolling
-              through multiple pages of results
+              through multiple pages of results.
             </li>
           </ol>
         </div>
