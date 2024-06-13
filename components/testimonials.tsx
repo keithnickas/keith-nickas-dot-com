3c818f1297a71230c25fea4d0912d6809d872fa6
Keith Nickas <keithnickas@gmail.com>
2024-06-13 11:48:32 -0700

    fix(home): adjust heading size to prevent line break

--- components/testimonials.tsx
+++ components/testimonials.tsx
@@ -11,7 +11,7 @@ export default function Testimonials() {
 
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
-            <h1 className="h2 mb-4">Discover What My Co-Workers Have to Say:</h1>
+            <h1 className="h3 mb-4">Discover What My Co-Workers Have to Say:</h1>
             <p className="text-xl text-gray-400">Genuine Testimonials Speak for Themselves</p>
           </div>
 
